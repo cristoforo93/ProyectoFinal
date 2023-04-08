97ee5fa792fed916931960c9cb1ed2a13248121f
cristoforo93 <ccastanedapaiz@gmail.com>
2023-04-07 19:18:26 -0600

    Plataforma
    Se analizan la plataforma con mayores ventas según región.

--- sql_data.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-#lista con todos los datos a ingresar a la base de datos
-
-sql_data = [*csv.DictReader(open('ProyectoFinal\Video_Games_Sales_as_at_22_Dec_2016.csv'))]
-
-print(len(sql_data))
